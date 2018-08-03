ae92ac5966e2bb63ea5a30a3f66624bc2bd31b80
Buheyra <buheyra1212@gmail.com>
2018-08-03 09:45:40 -0400

    Add files via upload

--- friedBeat.js
+++ friedBeat.js
@@ -1,21 +1,23 @@
 'use strict'
 
 var app = function(){
+    
 	const rows = document.querySelectorAll('.beatmaker__row');
   
-    
 	const buttons = document.querySelectorAll('.beatmaker__beat');
-    
-    
     
 	const indicator = document.querySelector('.beatmaker__indicator');
     
     console.log(indicator);
     
 	const playBut = document.querySelector('.beatmaker__play');
+    
 	const stopBut = document.querySelector('.beatmaker__stop');
+    
 	const bpmSelector = document.querySelector('.beatmaker__bpm');
+    
 	const preSelector = document.querySelector('.beatmaker_pre');
+    
 	const predefinedBeats = [
 								{
 									1:[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1],
@@ -41,52 +43,83 @@ var app = function(){
 								}
 							]
 	let leftPos = 200;
+    
 	let counter = 0;
+    
 	let timer;
+    
 	let fps = 4;
+    
 	let now;
+    
 	let then = Date.now();
+    
 	let interval;
+    
 	let delta;
+    
 	let isPlaying = false;
+    
 	let activeArray = [];
+    
 	let req;
 
 
 	function clickHandler(e) {
+        
 		if(e.target.tagName.toLowerCase() === 'button') {
+            
 			e.target.classList.toggle('active');
+            
 			e.target.firstChild.classList.toggle('active');
+            
 			activeArray.push(e.target);
+            
 		}else if(e.target.tagName.toLowerCase() === 'i') {
+            
 			e.target.parentNode.classList.toggle('active');
+            
 			e.target.classList.toggle('active');
+            
 			activeArray.push(e.target.parentNode);
 		}
 	}
 
 	function playSound(key) {
+        
 	  const audio = document.querySelector(`audio[data-key="${key}"]`);
+        
 	  if(!audio) return;
+        
 	  audio.currentTime = 0;
+        
 	  audio.play();
 	}
 
 	function playBeat() {
+        
 	    req = requestAnimationFrame(playBeat);
 	    
 	    if(this){
+            
 	    	if(!isPlaying) {
+                
 	    		this.style.display = "none";
+                
 	    		stopBut.style.display = "block";
+                
 	    		bpmSelector.setAttribute('disabled', true);
+                
 	    		preSelector.setAttribute('disabled', true);
+                
 	    		isPlaying = true;
 	    	}
 	    }
 
 	    interval = 1000/fps;
+        
 	    now = Date.now();
+        
 	    delta = now - then;
   
 	    if (delta > interval) {
@@ -96,62 +129,95 @@ var app = function(){
 	        if(counter <= 16) {
                 
 var currIndicator = counter == 0 ? document.querySelector('.indicator_1') : document.querySelector('.indicator_' + counter);
+                
 currIndicator.style.opacity = 0.1;
+                
 currIndicator.out = setTimeout(function(){ currIndicator.style.opacity = 0; }, interval);
+                
 rows.forEach(row => {
+    
 if(row.children[counter].classList.contains('active')) {
+    
 playSound(row.children[counter].getAttribute('data-key'));
 					}
 			 	})
 			 	//this takes care of the delay after the last beat is played
+                
 			 	counter == 16 ? counter = 1 : counter++;
 			}else{
+                
 				counter = 1;
 			}
 	    }
 	}
 
 	function setBPM(currentBPM) {
+        
 		switch(currentBPM) {
+                
 			case '60': 
+                
 				fps = 4;
 			break;
+                
 			case '120':
+                
 				fps = 8;
 			break;
+                
 			case '140':
+                
 				fps = 10;
 			break;
 		}
 	}
 
 	function handleBPMChange(e) {
+        
 		var el = e.target;
+        
 		var currentBPM = el.options[el.selectedIndex].dataset.bpm;
+        
 		setBPM(currentBPM);
 	}
 
 	function handlePreChange(e) {
+        
 		var el = e.target;
+        
 		var currentPRE = el.options[el.selectedIndex].dataset.pre;
+        
 		if(activeArray.length > 0) {
+            
 			activeArray.forEach(function(el) {
+                
 				el.classList.remove('active');
 			})
 		}
 		if (Number(currentPRE) === 1){
+            
 			setBPM('120');
+            
 			bpmSelector.selectedIndex = 1;
+            
 		}else{
 			setBPM('60');
 		}
+        
 		var rows = document.querySelectorAll('.beatmaker__row');
+        
 		var counter = 1;
+        
 		rows.forEach(function(el) {
+            
 			var child = el.children;
+            
 			[].forEach.call(child, function(childEl, index) {
+                
 				if(predefinedBeats[Number(currentPRE)][counter][index] === 1){
+                    
 					child[index].classList.add('active');
+                    
 					activeArray.push(child[index]);
 				} 
 			})
@@ -160,20 +226,31 @@ playSound(row.children[counter].getAttribute('data-key'));
 	}
 
 	function stopBeat() {
+        
 		window.cancelAnimationFrame(req);
+        
 		if(isPlaying) {
+            
     		this.style.display = "none";
+            
     		playBut.style.display = "block";
+            
     		isPlaying = false;
+            
     		bpmSelector.removeAttribute('disabled');
+            
 	    	preSelector.removeAttribute('disabled');
     	}
 	}
 	
 	buttons.forEach(button => button.addEventListener('click', clickHandler));
+    
 	playBut.addEventListener('click', playBeat);
+    
 	stopBut.addEventListener('click', stopBeat);
+    
 	bpmSelector.addEventListener('change', handleBPMChange);
+    
 	preSelector.addEventListener('change', handlePreChange);
 };
 
